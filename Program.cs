74122ae415b502cc94d47cbb6290cab5b0771ba4
Joey Tapic <joey.tapic@gmail.com>
2022-04-28 09:50:10 +0800

    Intitial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursion r = new Recursion();
+            r.calculate(4);
+        }
+    }
+}
---
+++ Recursion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DSAlgorithms
+{
+    class Recursion
+    {
+        public void calculate(int n)
+
+        {
+
+             for (int i=1; i <= n; i++)
+            {
+               int h = i * i;
+                Console.WriteLine(h);
+
+            }
+            Console.WriteLine();
+            while (n>0)
+            {
+                int k = n * n;
+                Console.WriteLine(k);
+                n = n - 1;
+            }
+            
+
+           
+        }
+
+    }
+}
