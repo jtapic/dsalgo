ccaf5a6eeabb14b9a78b2849866a79e18bfece28
Joey Tapic <joey.tapic@gmail.com>
2022-04-29 16:58:07 +0800

    Factorial Class added

---
+++ Factorial.cs
@@ -0,0 +1,20 @@
+﻿namespace DSAlgorithms
+{
+    class Factorial
+    {
+        public int FactorialIterative(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i<= n; i++)
+                 fact *= i;
+            return fact;
+        }
+
+        public int FactorialRecursive(int n)
+        {
+            if(n == 0)
+                return 1;
+            return FactorialRecursive(n-1)*n;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,19 +6,27 @@ namespace DSAlgorithms
     {
         static void Main(string[] args)
         {
+
+            //Factorial
+            Factorial f = new Factorial();
+            Console.WriteLine("Factorial: "+f.FactorialIterative(5));
+            Console.WriteLine("Factorial: "+f.FactorialRecursive(5));
+
+
+            // Sum of N 
+            //SumOfN s = new SumOfN();
+            //int sum = s.SumN(5);
+            //Console.WriteLine("Sum: "+ sum);
+            //sum = s.SumNIterative(10);
+            //Console.WriteLine("sum: "+ sum);
+            //sum =  s.SumNRecursion(15);
+            //Console.WriteLine("sum" + sum);
             // Recursion
             // Recursion r = new Recursion();
             // r.calculate(5);
             //  r.CalculateRecursion(4);
 
-            // Sum of N 
-            SumOfN s = new SumOfN();
-            int sum = s.SumN(5);
-            Console.WriteLine("Sum: "+ sum);
-            sum = s.SumNIterative(10);
-            Console.WriteLine("sum: "+ sum);
-            sum =  s.SumNRecursion(15);
-            Console.WriteLine("sum" + sum);
+            
             
         } 
     }
