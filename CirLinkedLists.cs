16f32278323c6ffab1027bf8ca0cc1ee87842ad9
Joey Tapic <joey.tapic@gmail.com>
2022-05-24 10:48:35 +0800

    Created: CirLinkedLists Class

---
+++ CirLinkedLists.cs
@@ -0,0 +1,16 @@
+﻿namespace DSAlgorithms
+{
+    public class CirLinkedLists
+    {
+        private Node head;
+        private Node tail;
+        private int Size;
+
+        public CirLinkedLists()
+        {
+            head = null;
+            tail = null;
+            Size = 0;
+        }
+    }
+}
